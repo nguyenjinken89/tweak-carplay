086a85bde8aaa54337e36455c2b2f931132a39bd
nguyenjinken89 <nguyenjinken89@gmail.com>
2026-09-09 09:46:44 +0700

    Define PSSpecifier and PSListController interfaces
    Added PSSpecifier and PSListController interfaces to manage preferences.

--- carplaymasterprefs/CPMRootListController.h
+++ carplaymasterprefs/CPMRootListController.h
@@ -3,7 +3,19 @@
 //  CarPlayMasterPrefs
 //
 
-#import <Preferences/PSListController.h>
+#import <UIKit/UIKit.h>
+
+@interface PSSpecifier : NSObject
+@property (nonatomic, strong) id target;
+@property (nonatomic, strong) NSString *name;
+@end
+
+@interface PSListController : UIViewController
+- (id)loadSpecifiersFromPlistName:(NSString *)name target:(id)target;
+- (void)reloadSpecifiers;
+- (void)setPreferenceValue:(id)value specifier:(PSSpecifier *)specifier;
+- (id)readPreferenceValue:(PSSpecifier *)specifier;
+@end
 
 @interface CPMRootListController : PSListController
 
